0dfc94962068569ddd7cee2113d7db2e05991952
Mileni Lelis Ramos Servulo <lelis.engineer@yahoo.com>
2026-04-24 22:45:21 -0300

    Update f&w.py

---
+++ f&w.py
@@ -0,0 +1,69 @@
+#Desenvolver um programa que exiba a mensagem ‘Olá Turma!!!’ dez vezes na tela.
+for i in range (1,11)
+  print(f"{i} - Olá Tuma!!!"):
+
+while < 11
+  i = 1
+  print (f"{i} - Olá Tuma!!!"):
+  i += 1
+--------------------------------------------------------------------------------------------------------
+#Desenvolver um programa que exiba na tela números inteiros de 1 até 20.
+
+for i in range (1,21):
+  print(i)
+
+i = 1
+while < 21 :
+  print(i)
+  i += 1
+---------------------------------------------------------------------------------------------------------
+#Elaborar um programa que apresente como resultado os quadrados armazenados na memória dos números
+inteiros existentes na faixa de valores de 15 a 50. Resultado: 2500
+
+for i in range(15, 51):
+    print(i ** 2)
+
+i=15
+while < 51:
+  print(i ** 2)
+  i += 1
+---------------------------------------------------------------------------------------------------------
+#Desenvolver um programa que exiba na tela a soma dos cinquenta primeiros números naturais ( 1 + 2 + 3 + ...
++ 49 + 50). Resultado: 1275
+s = 0
+for i in range (1,51):
+   s += i
+print(s)
+
+s = 0
+i = 1
+while < 51:
+  s += i
+  i += 1
+ print(s)
+---------------------------------------------------------------------------------------------------------------
+#Desenvolver um programa que exiba os números inteiros pares entre 1 e 30.
+
+for i in range (2,30,2):
+  print(i)
+
+i = 2
+while i < 31:
+  print(i)
+  i += 2
+----------------------------------------------------------------------------------------------------------------
+#Elaborar um programa que apresente o somatório dos valores pares existentes na faixa de 1 até 50.
+s=0
+for i in range (2,50,2):
+   s += i
+  print(s)
+
+i=2
+s=0
+
+while < 51:
+   s += i
+   i += 2
+  print(s)
+
+  
